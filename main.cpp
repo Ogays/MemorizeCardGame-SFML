c1644a9f6fdccf2f9c1ab2fb5ccde16f011cb877
Axmad Saugi <saugi.axmad@gmail.com>
2025-09-07 16:56:55 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿/*
+ * File:   main.cpp
+ * Author: ogay
+ *
+ * Created on August 26, 2025, 9:06 AM
+ */
+
+#include "include/DEFINITION.hpp"
+#include "include/Game.hpp"
+
+int main() {
+
+    Ogay::Game game(ScreenWidth, ScreenHeight, "Simple State Change");
+    return 0;
+}
